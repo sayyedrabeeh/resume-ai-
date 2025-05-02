3067b741c7816f63fede94ee0c149eb1c9abe24e
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-05-02 20:05:54 +0530

    making resume builder

--- ai_resume_matcher/backend/apps/resumes/views.py
+++ ai_resume_matcher/backend/apps/resumes/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from  docx import Document
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class GenerateResumeView(APIView):
+     permission_classes = [IsAuthenticated]
+
+     def post(self,request):
+          data = request.data
+          document =Document()
+
+          document.add_heading(data.get_name('name',''),0)
+
+          document.add_paragraph(f"Email: {data.get('email', '')}")
+          document.add_paragraph(f"Phone: {data.get('phone', '')}")
+          document.add_paragraph(f"Summary:\n{data.get('summary', '')}")
+
+          document.add_heading('Skills',level=1)
+          for skill in data.get('skill',[]):
+               document.add_paragraph(skill, style='List Bullet')
+               
+          document.add_heading('Experience',level=1)
+          for exp in data.get('Experience',[]):
+               document.add_paragraph(f"{exp['title']} at {exp['company']} ({exp['duration']})")
+               document.add_paragraph(exp['description'])
